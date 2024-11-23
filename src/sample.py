447d2ce2be74be8a3141531087c81de5186c7882
tcastillo_ubuntu <theocastillo@yahoo.com>
2024-11-23 12:55:45 +0100

    up

--- src/sample.py
+++ src/sample.py
@@ -5,16 +5,16 @@ from src.model.diffusion import Diffusion
 from src.model.unet import UNet
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-unet = UNet().to(device)
-unet.load_state_dict(torch.load("../checkpoints/DDPM_Uncondtional_cat/ckpt_395.pt", weights_only=True))
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    unet = UNet().to(device)
+    unet.load_state_dict(torch.load("../checkpoints/DDPM_Uncondtional_cat/ckpt_395.pt", weights_only=True))
 
-seed = torch.randint(0, 100000, (1,)).item()
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+    seed = torch.randint(0, 100000, (1,)).item()
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
-
-diffusion = Diffusion(noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=64, device=device)
-diffusion.sample_with_step(unet, 1,"results/image_gif3")
+    diffusion = Diffusion(noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=64, device=device)
+    diffusion.sample_with_step(unet, 1, "results/image_gif3")
 
